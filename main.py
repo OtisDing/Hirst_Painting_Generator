f49f5e4108f0107d75ef5d4c47812e2884c1c0c5
OtisDing <93888172+OtisDing@users.noreply.github.com>
2023-05-13 20:56:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import turtle as t
+import random
+
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+colourList = [(213, 154, 96), (52, 107, 132), (179, 77, 31), (202, 142, 31), (115, 155, 171), (124, 79, 99), (122, 175, 156), (226, 198, 131), (192, 87, 108), (11, 50, 64), (55, 38, 19), (45, 168, 126), (47, 127, 123), (200, 121, 143), (168, 21, 29), (228, 92, 77), (244, 162, 160), (38, 32, 35), (2, 25, 24), (78, 147, 171), (170, 23, 18), (19, 79, 90), (101, 126, 158), (235, 166, 171), (177, 204, 185), (49, 62, 84)]
+
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+numDots = 100
+
+
+for i in range(1, numDots + 1):
+    tim.dot(20, random.choice(colourList))
+    tim.forward(50)
+
+    if i % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
